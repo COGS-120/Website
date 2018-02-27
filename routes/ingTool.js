63000221b22a761b6e513b0dd82d05d69601e661
Kenneth Nguyen <kdn004@ucsd.edu>
2018-02-26 19:53:23 -0800

    finished all food

--- routes/ingTool.js
+++ routes/ingTool.js
@@ -5,9 +5,9 @@ exports.view = function(req, res) {
 	var dataWeWant;
 	var index = 0;
 
-	console.log(recipeData);
+
 	for (index = 0; index < recipeData.length; index++) {
-		if (recipeData[index].name === name){
+		if (recipeData[index].name == name){
 			dataWeWant = recipeData[index];
 			break;
 		}
@@ -16,23 +16,14 @@ exports.view = function(req, res) {
 			console.log("no we couldnt find " + name);
 			break;
 		}
-
-		index++;
 	}
 
 
-if (typeof dataWeWant !== 'undefined') {
-    // the variable is defined
+ 
     	res.render("ingTool", {
 		"name" : name,
 		"tools": dataWeWant.tools,
 		"ingredients": dataWeWant.ingredients
 	});
-}
 
-else {
-		res.render("ingTool", {
-		"name" : name
-	});
-}
 };
